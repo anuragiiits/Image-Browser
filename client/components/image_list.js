7dc877c69c43951a9917220ec4207a032adefe5f
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-11 12:08:39 +0530

    Initial Commit

---
+++ client/components/image_list.js
@@ -0,0 +1,13 @@
+//Create the component
+//Step 1: import React
+import React from 'react';
+//Step 2: Create the component
+const ImageList = () =>{
+    return (
+        <ul>
+            <li>Image Detail</li>
+        </ul>
+    );
+};
+//Step 3: Export the component created
+export default ImageList;
---
+++ client/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ImageList from './components/image_list';
+const App = () =>{
+  return (
+    <div>
+      <ImageList/>
+    </div>
+  );
+};
+Meteor.startup( ()=>{
+ReactDOM.render(<App/>, document.querySelector('.container'));
+});
