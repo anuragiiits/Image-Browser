fce27b3a4bfdd4f3f28c6f6fe9860515a045e1c8
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-15 00:51:37 +0530

    Implimenting state

--- client/main.js
+++ client/main.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom';
 import ImageList from './components/image_list';
 import axios from 'axios';
 class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = { images:[] }
+  }
+
   componentWillMount(){
     //fantastic place to  load all data to be rendered in webpage
     console.log('App is about to be rendered');
     axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-      .then((response) => console.log(response));
-    //A problem to solve is that it takes some time to receive the response from the server
-    //Hence we need to do something to stop rendering till we dont receive response.
+      .then((response) => this.setState({ images:response.data.data }));
+      //NEVER DO THIS : this.state.images = [{}, {}]
   }
+
+  //whenever state is updated, the component is automatically rendered again
+  //hence, console is called 2 times
    render(){
+     console.log(this.state.images)
     return (
       <div>
         <ImageList/>
