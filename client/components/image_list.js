3bd08026756408f4ca944548d094c7a0f1b0370b
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-14 11:51:38 +0530

    Map function

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,13 +1,28 @@
 //Create the component
 //Step 1: import React
 import React from 'react';
+import ImageDetail from './image_detail';
 //Step 2: Create the component
+
+const IMAGES = [
+    {title:'pen', link:'https://dummyimage.com/600x400'},
+    {title:'paper', link:'https://dummyimage.com/600x400'},
+    {title:'book', link:'https://dummyimage.com/600x400'},
+    {title:'laptop', link:'https://dummyimage.com/600x400'}
+];
+
 const ImageList = () =>{
+
+    const RenderedImages = IMAGES.map(function(image){
+        return (
+            <ImageDetail/> 
+        );
+    });
     return (
         <ul>
-            <li>Image Detail</li>
+            {RenderedImages}
         </ul>
-    );
+    ); 
 };
 //Step 3: Export the component created
 export default ImageList;
