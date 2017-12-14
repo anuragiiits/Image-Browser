cca97f8fe7b4d392cdcadc055815e5f65ddd98e8
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-14 17:08:21 +0530

    Added the get request to images at a server and a problem in mentioned to solve

--- client/main.js
+++ client/main.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import ImageList from './components/image_list';
+import axios from 'axios';
 class App extends Component{
+  componentWillMount(){
+    //fantastic place to  load all data to be rendered in webpage
+    console.log('App is about to be rendered');
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then((response) => console.log(response));
+    //A problem to solve is that it takes some time to receive the response from the server
+    //Hence we need to do something to stop rendering till we dont receive response.
+  }
    render(){
     return (
       <div>
